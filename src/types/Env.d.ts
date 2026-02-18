fa3e8a5e599dfc20b8eb8fe437542d9fd8c132e2
Hariomland <98735078+Hariomland@users.noreply.github.com>
2026-02-18 11:20:30 +0530

    H

---
+++ src/types/Env.d.ts
@@ -0,0 +1,9 @@
+export interface EnvConfig {
+  DISCORD_TOKEN: string;
+  CLIENT_ID: string;
+  PREFIX: string;
+  LAVALINK_HOST: string;
+  LAVALINK_PORT: number;
+  LAVALINK_PASSWORD: string;
+  EXPRESS_PORT: number;
+}
